232c3192a46b67fec87bb981a319f820144c2351
LukasDekan <Lukas.dekan24@student.cart.org>
2025-02-04 14:52:39 -0800

    First few lines of code

---
+++ index.js
@@ -0,0 +1,67 @@
+list = {
+    friendName:  prompt("What is your friend's name?"),
+    type: prompt("What are you rewarding him for?"),
+    title: prompt("How skilled is your friend in this area?"),
+    issuerName: (prompt("What is your name?"))
+};
+
+document.write(list.friendName);
+document.write(list.type);
+document.write(list.title);
+document.write(list.issuerName);
+
+document.getElementById('output1').innerHTML = 'Some text!';
+
+
+
+
+
+
+
+
+
+
+
+
+// function calcPrompt(){
+   
+// let currentDate = Number
+// let equationA = num1 + num2;
+// let equationB = num1 - num2;
+// let equationC = num1 * num2;
+// let equationD = num1 / num2;
+// if(askEquation == "Add"){
+//     document.write(equationA);
+// }
+// else if(askEquation == "Subtract"){
+//     document.write(equationB);
+// }
+// else if(askEquation == "Multiply"){
+//     document.write(equationC);
+// }
+// else if(askEquation == "Devide"){
+//     document.write(equationD);
+// }
+// else if(askEquation == "+"){
+//     document.write(equationA);
+// }
+// else if(askEquation == "-"){
+//     document.write(equationB);
+// }
+// else if(askEquation == "*"){
+//     document.write(equationC);
+// }
+// else if(askEquation == "/"){
+//     document.write(equationD);
+// }
+// else if(askEquation == "Plus"){
+//     document.write(equationA);
+// }
+// else if(askEquation == "Minus"){
+//     document.write(equationB);
+// }
+// else{
+//     document.write("You entered an invalid responce.")
+// }
+// }
+// calcPrompt();
